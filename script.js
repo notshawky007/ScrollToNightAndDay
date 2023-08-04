5d615df9a21519a1533f876e50c82dbfe0ce94fe
shawky_53 <mostafashawky100000@gmail.com>
2023-08-04 05:59:39 +0300

    done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// JavaScript
 let stars = document.getElementById("stars");
 let moon = document.getElementById("moon");
 let mountain = document.getElementById("mountain");
@@ -14,15 +15,30 @@ window.addEventListener("scroll", function () {
   mountain.style.top = value * 0.5 + "px";
   mountain2.style.top = value * 0.5 + "px";
   river.style.top = value * 0.15 + "px";
-  boat.style.top = value * 0.15 + "px";
-  title.style.fontSize = value * +"px";
-  if (scrollY >= 67) {
-    title.style.fontSize = 67 + "px";
+
+  // Parallax effect for the boat (move in the opposite direction of scroll)
+  boat.style.top = value * -0.15 + "px";
+
+  // Change background color based on scroll position
+  const maxScroll = 1000; // Adjust the maximum scroll value to change the background color
+  const backgroundColor = `rgba(255, 0, 0, ${value / maxScroll})`; // Change red color based on scroll position
+  document.body.style.backgroundColor = backgroundColor;
+
+  // Set the initial font size of the title
+  title.style.fontSize = "20px";
+
+  if (scrollY >= 400) {
+    // Set the title's font size to a fixed value when the scroll position exceeds 400
+    title.style.fontSize = "30px";
     title.style.position = "fixed";
-    if (scrollY >= 400) {
-      title.style.display = "none";
-    } else {
-      title.style.display = "block";
-    }
+    title.style.top = "0";
+    title.style.left = "50%";
+    title.style.transform = "translateX(-50%)";
+
+    // Hide the title when the scroll position exceeds 400
+    title.style.display = "none";
+  } else {
+    title.style.display = "block";
+    title.style.position = "static";
   }
 });
